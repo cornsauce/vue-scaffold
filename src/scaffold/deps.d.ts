00aa826eb56e53600dfe6bcb9107a2381fbfec6e
cornsauce <cornsauce@163.com>
2020-04-10 23:14:18 +0800

    update .d.ts

--- src/scaffold/deps.d.ts
+++ src/scaffold/deps.d.ts
@@ -1,5 +1,7 @@
 import {EventEmitter} from 'events';
 import {Application} from '@/scaffold/core/application';
+import VueRouter from 'vue-router';
+import VueI18n from 'vue-i18n';
 
 declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
@@ -11,6 +13,15 @@ declare module 'vue/types/options' {
 declare module 'vue/types/vue' {
   interface Vue {
     $app?: Application;
+    $bus?: EventEmitter;
+  }
+}
+
+declare module 'vuex' {
+  interface Store<S> {
+    $app: Application;
+    $route: VueRouter;
+    $i18n: VueI18n;
     $bus?: EventEmitter;
   }
 }
